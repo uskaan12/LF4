a989d22909f10d533749a64f56cc1eb191ad25ee
Johoski <74900059+Johoski@users.noreply.github.com>
2022-11-17 08:19:11 +0100

    Update beispiel1.java

--- src/main/java/bz/berufsschule/while_schleife/beispiel1.java
+++ src/main/java/bz/berufsschule/while_schleife/beispiel1.java
@@ -1,4 +1,19 @@
 package bz.berufsschule.while_schleife;
 
+/*
+Man kann jede for-Schleife als while-Schleife programmieren.
+Schreibe die folgende for-Schleife als while-Schleife:
+for (int i =1; i<=10; i=i+1) {
+            System.out.println(i);
+}
+Diese Schleife wird 10-mal durchlaufen und gibt die Werte von 0 bis 9 aus
+ */
 public class beispiel1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
 }
