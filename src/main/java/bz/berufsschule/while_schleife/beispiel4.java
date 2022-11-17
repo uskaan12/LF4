cc6c63f606f35d4e11b05e2bb0d4500a64022299
Johoski <74900059+Johoski@users.noreply.github.com>
2022-11-17 09:08:12 +0100

    Update beispiel4.java

--- src/main/java/bz/berufsschule/while_schleife/beispiel4.java
+++ src/main/java/bz/berufsschule/while_schleife/beispiel4.java
@@ -1,5 +1,7 @@
 package bz.berufsschule.while_schleife;
 
+import javax.swing.*;
+
 /*
 Erstelle ein Programm, das eine Dezimal in eine Binärzahl umwandelt. Zum Beispiel soll aus der
 Dezimalzahl 8 die Binärzahl 100 werden.
@@ -8,6 +10,17 @@ oder ob es beendet werden soll. möchte er ds Programm wiederholen, so startet e
 */
 public class beispiel4 {
     public static void main(String[] args) {
+        String eingabe = JOptionPane.showInputDialog("Gib eine Dezimalzahl ein:");
+        int zahl = Integer.parseInt(eingabe);
 
+        String ergebnis = "";
+
+        while (zahl > 0) {
+            int rest = zahl % 2;
+            ergebnis = rest + ergebnis;
+            //zahl /=2 alternative schreibweise für zahl = zahl / 2;
+            zahl = zahl / 2;
+        }
+        System.out.println(eingabe + " entspricht der Binärzahl " + ergebnis);
     }
 }
