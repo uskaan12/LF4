3ba3cf567e4e6ef4a645bc350d5435768820ee9c
Johoski <74900059+Johoski@users.noreply.github.com>
2022-11-17 09:01:40 +0100

    Update beispiel4.java

--- src/main/java/bz/berufsschule/while_schleife/beispiel4.java
+++ src/main/java/bz/berufsschule/while_schleife/beispiel4.java
@@ -1,7 +1,13 @@
 package bz.berufsschule.while_schleife;
 
+/*
+Erstelle ein Programm, das eine Dezimal in eine Binärzahl umwandelt. Zum Beispiel soll aus der
+Dezimalzahl 8 die Binärzahl 100 werden.
+Bevor das Programm beendet wird, soll der Benutzer gefragt werden ob er ds Programm wiederholen möchte
+oder ob es beendet werden soll. möchte er ds Programm wiederholen, so startet es von vorne, andernfalls wird es beendet.
+*/
 public class beispiel4 {
     public static void main(String[] args) {
-        
+
     }
 }
