5f703ad6a672507d67a886c96aa416e627ea906b
Johoski <74900059+Johoski@users.noreply.github.com>
2022-11-17 09:23:45 +0100

    Update leiter_und_leitern.java

--- src/main/java/bz/berufsschule/while_schleife/leiter_und_leitern.java
+++ src/main/java/bz/berufsschule/while_schleife/leiter_und_leitern.java
@@ -1,7 +1,121 @@
 package bz.berufsschule.while_schleife;
 
+import bz.berufsschule.for_schleife.wuerfelspiel3;
+
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class leiter_und_leitern {
     public static void main(String[] args) {
+        int spieler1 = 0;
+        int spieler2 = 0;
+        String spieler1Balken = " ";
+        String spieler2Balken = " ";
 
+        for (int i = 0; i < 10; i++) {
+            Random random = new Random();
+            Random random2 = new Random();
+            int rnd = random.nextInt(6) + 1;
+            int rnd2 = random2.nextInt(6) + 1;
+            spieler1 = spieler1 + rnd;
+            spieler2 = spieler2 + rnd2;
+            for (int j = 0; j < rnd; j++) {
+                spieler1Balken += "-";
+            }
+            for (int j = 0; j < rnd2; j++) {
+                spieler2Balken += "-";
+            }
+            if (spieler1 == 19) {
+                spieler1 = 66;
+                for (int j = 0; j < 47; j++) {
+                    spieler1Balken += "-";
+                }
+            } else if (spieler1 == 67) {
+                spieler1 = 100;
+                for (int j = 0; j < 33; j++) {
+                    spieler1Balken += "-";
+                }
+            } else if (spieler1 == 32) {
+                spieler1 = 53;
+                for (int j = 0; j < 21; j++) {
+                    spieler1Balken += "-";
+                }
+            } else if (spieler1 == 73) {
+                spieler1 = 91;
+                for (int j = 0; j < 18; j++) {
+                    spieler1Balken += "-";
+                }
+            } else if (spieler1 == 88) {
+                spieler1 = 71;
+                spieler1Balken = spieler1Balken.substring(0, spieler1);
+            } else if (spieler1 == 74) {
+                spieler1 = 52;
+                spieler1Balken = spieler1Balken.substring(0, spieler1);
+            } else if (spieler1 == 46) {
+                spieler1 = 12;
+                spieler1Balken = spieler1Balken.substring(0, spieler1);
+            } else if (spieler1 == 25) {
+                spieler1 = 6;
+                spieler1Balken = spieler1Balken.substring(0, spieler1);
+            }
+            if (spieler2 == 19) {
+                spieler2 = 66;
+                for (int j = 0; j < 47; j++) {
+                    spieler2Balken += "-";
+                }
+            } else if (spieler2 == 67) {
+                spieler2 = 100;
+                for (int j = 0; j < 33; j++) {
+                    spieler2Balken += "-";
+                }
+            } else if (spieler2 == 32) {
+                spieler2 = 53;
+                for (int j = 0; j < 21; j++) {
+                    spieler2Balken += "-";
+                }
+            } else if (spieler2 == 73) {
+                spieler2 = 91;
+                for (int j = 0; j < 18; j++) {
+                    spieler2Balken += "-";
+                }
+            } else if (spieler2 == 88) {
+                spieler2 = 71;
+                spieler2Balken = spieler2Balken.substring(0, spieler2);
+            } else if (spieler1 == 74) {
+                spieler2 = 52;
+                spieler2Balken = spieler1Balken.substring(0, spieler2);
+            } else if (spieler2 == 46) {
+                spieler2 = 12;
+                spieler2Balken = spieler2Balken.substring(0, spieler2);
+            } else if (spieler1 == 25) {
+                spieler2 = 6;
+                spieler2Balken = spieler1Balken.substring(0, spieler2);
+            }
+            System.out.print("Spieler1 hat:" + spieler1 + "Punkte. ||");
+            System.out.print("Spieler2 hat:" + spieler2 + "Punkte. ||");
+            System.out.println(" ");
+
+            System.out.println("Spieler1:" + spieler1Balken);
+            System.out.println("Spieler2" + spieler2Balken);
+
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(wuerfelspiel3.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+        System.out.println(" ");
+
+
+        if (spieler1 < spieler2) {
+            System.out.println("Spieler2 hat mit " + spieler2 + " Punkten das Spiel gewonnen!");
+        } else if (spieler1 > spieler2) {
+            System.out.println("Spieler1 hat mit " + spieler1 + " Punkten das Spiel gewonnen!");
+        } else {
+            System.out.println("Die Spieler haben gleiche Punkte. Unendschieden!");
+        }
     }
 }
