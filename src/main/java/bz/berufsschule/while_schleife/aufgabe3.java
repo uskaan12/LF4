4e5bc04d75cf93c13becfa3720aa28da1a679972
Johoski <74900059+Johoski@users.noreply.github.com>
2022-11-24 09:31:42 +0100

    d

---
+++ src/main/java/bz/berufsschule/while_schleife/aufgabe3.java
@@ -0,0 +1,21 @@
+package bz.berufsschule.while_schleife;
+
+import javax.swing.*;
+
+public class aufgabe3 {
+    public static void main(String[] args) {
+        int kombination = 666;
+        boolean kombination_falsch = true;
+        int z = 0;
+        while (kombination_falsch = true && z < 5) {
+            String eingabe = JOptionPane.showInputDialog("Geben sie die Kombination ein");
+            int eingabe_als_zahl = Integer.parseInt(eingabe);
+
+            if (eingabe_als_zahl == kombination) {
+                kombination_falsch = false;
+            } else {
+                System.out.println("");
+            }
+        }
+    }
+}
