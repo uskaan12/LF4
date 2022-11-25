ac6462d0834678cfc9f483f67a3cccf8e9c86855
Johoski <74900059+Johoski@users.noreply.github.com>
2022-11-25 09:28:56 +0100

    Update uebung_fibonaccifolge.java

--- src/main/java/bz/berufsschule/while_schleife/uebung_fibonaccifolge.java
+++ src/main/java/bz/berufsschule/while_schleife/uebung_fibonaccifolge.java
@@ -1,10 +1,28 @@
 package bz.berufsschule.while_schleife;
+
+import javax.swing.*;
+
 /*
 Übung 3
 Fibonaccifolge --> 1,1,2,3,5,8,13,21,34,...
  */
 public class uebung_fibonaccifolge {
     public static void main(String[] args) {
+        String eingabe = JOptionPane.showInputDialog("Geben sie die Obergrenze ein");
+        int grenze = Integer.parseInt(eingabe);
+        int a = 1, b = 1, summe;
+        boolean wiederholen = true;
+        System.out.print("1, 1");
+        while (wiederholen) {
+            summe = a + b;
+            a = b;
+            b = summe;
+            if (summe > grenze) {
+                wiederholen = false;
+            } else {
+                System.out.print(", " + summe);
+            }
 
+        }
     }
 }
