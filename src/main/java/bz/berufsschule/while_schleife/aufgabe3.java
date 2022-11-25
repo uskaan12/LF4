f75ef43d75ecbd5a33e1952eb7ad5a540b2eaa52
Johoski <74900059+Johoski@users.noreply.github.com>
2022-11-25 08:11:39 +0100

    Update aufgabe3.java

--- src/main/java/bz/berufsschule/while_schleife/aufgabe3.java
+++ src/main/java/bz/berufsschule/while_schleife/aufgabe3.java
@@ -2,15 +2,15 @@ package bz.berufsschule.while_schleife;
 
 public class aufgabe3 {
     public static void main(String[] args) {
-       for (int j=1;j<10;j++) {
-           for (int i = 1; i < 10; i++) {
-               if (i == 9) {
-                   System.out.print(i*j);
-               } else {
-                   System.out.print(i*j + ", ");
-               }
-           }
-           System.out.println("");
-       }
+        for (int j = 1; j < 10; j++) {
+            for (int i = 1; i < 10; i++) {
+                if (i == 9) {
+                    System.out.print(i * j);
+                } else {
+                    System.out.print(i * j + ", ");
+                }
+            }
+            System.out.println("");
+        }
     }
 }
