7e23b9efea23661ef96ef775c1f278dd73de584b
Johoski <74900059+Johoski@users.noreply.github.com>
2022-11-26 12:11:03 +0100

    addded arrays + example

---
+++ src/main/java/bz/berufsschule/arrays/bsp1.java
@@ -0,0 +1,8 @@
+package bz.berufsschule.arrays;
+
+public class bsp1 {
+    public static void main(String[] args) {
+        String[] cars = {"Camaro", "Corvette", "Tesla"};
+        System.out.println(cars[0]);
+    }
+}
