d7c01b2fd147e7f2d2e24e011a497dea11e353d5
Johoski <74900059+Johoski@users.noreply.github.com>
2022-12-20 11:23:34 +0100

    arrays

---
+++ src/main/java/bz/berufsschule/arrays/bsp2.java
@@ -0,0 +1,25 @@
+package bz.berufsschule.arrays;
+
+/*
+Erstelle ein Integer-Array der Länge 10 und fülle es mit Zufallszahlen zwischen 1 und 100.
+Anschließend zähle wie viele Zahlen größer als 50 sind und gib das Ergebnis in der Konsole aus.
+ */
+public class bsp2 {
+    public static void main(String[] args) {
+        // Erstelle ein Integer-Array der Länge 10
+        int[] array = new int[10];
+        // Fülle es mit Zufallszahlen zwischen 1 und 100
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100) + 1;
+        }
+        // Zählt wie viele Zahlen größer als 50 sind
+        int counter = 0;
+        for (int num : array) {
+            if (num > 50) {
+                counter++;
+            }
+        }
+        // Gibt das Ergebnis in der Konsole aus
+        System.out.println("Im Array sind "+counter + " Zahlen größer als 50.");
+    }
+}
