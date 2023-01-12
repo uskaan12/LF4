84b34e602cf45f61b03f89532ba14aa1cc78584e
Johoski <hanssenoner@gmail.com>
2023-01-12 21:14:20 +0100

    sus

---
+++ src/main/java/bz/berufsschule/string_operationen/aufgabe_5_3.java
@@ -0,0 +1,18 @@
+package bz.berufsschule.string_operationen;
+
+import javax.swing.*;
+
+/*
+Eine Zeichenkette wird eingelesen. Aus dieser Zeichenkette sollen zwei neue Variablen gebildet werden,
+welche den eingegebenen Text jeweils in Klein- und Großbuchstaben beinhalten.
+Diese Variablen sollen am Bildschirm ausgegeben werden.
+ */
+public class aufgabe_5_3 {
+    public static void main(String[] args) {
+        String text = JOptionPane.showInputDialog("Geben Sie eine Zeichenkette ein: ");
+        String textKlein = text.toLowerCase();
+        String textGross = text.toUpperCase();
+        System.out.println("Kleinbuchstaben: " + textKlein);
+        System.out.println("Großbuchstaben: " + textGross);
+    }
+}
