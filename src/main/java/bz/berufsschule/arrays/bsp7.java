4716e1d266296c2de34e9ea22e0097397669a4d1
Johoski <74900059+Johoski@users.noreply.github.com>
2023-01-12 09:17:33 +0100

    fixed issues with submodule

--- src/main/java/bz/berufsschule/arrays/bsp7.java
+++ src/main/java/bz/berufsschule/arrays/bsp7.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 public class bsp7 {
     public static void main(String[] args) {
+        int zaehler_vokale = 0, zaehler_konsonanten = 0;
         //Benutzer gibt das Wort ein
         String wort = JOptionPane.showInputDialog("Geben Sie ein Wort ein: ");
 
@@ -12,9 +13,12 @@ public class bsp7 {
 
         for (int i = 0; i < wortArray.length; i++) {
             if (wortArray[i] == 'a' || wortArray[i] == 'e' || wortArray[i] == 'i' || wortArray[i] == 'o' || wortArray[i] == 'u') {
-
+                zaehler_vokale++;
+            } else {
+                zaehler_konsonanten++;
             }
         }
-
+        //Ausgabe
+        JOptionPane.showMessageDialog(null, "Das Wort " + wort + " enthält " + zaehler_vokale + " Vokale und " + zaehler_konsonanten + " Konsonanten.");
     }
 }
