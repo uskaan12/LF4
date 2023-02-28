94897b678fb78597950fc23202be78ffe6151d3e
Johoski <74900059+Johoski@users.noreply.github.com>
2023-02-28 11:23:29 +0100

    s

---
+++ src/main/java/bz/berufsschule/static_methods/taschenrechner.java
@@ -0,0 +1,15 @@
+package bz.berufsschule.static_methods;
+
+public class taschenrechner {
+    //Eine Methode um zwei Werte zu addieren
+    public static double taschenrechner_addition(int num1, int num2){
+
+    }
+    //Eine Methode um zwei Werte zu subtrahieren
+
+    //Eine Methode um zwei Werte zu multiplizieren
+
+    //Eine Methode um zwei Werte zu dividieren
+
+    //Eine Main Methode in der der Benutzer Werte eingibt
+}
